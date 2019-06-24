e15cf5bef99a56760ef59dd3a03a4f1602057c31
暮雪 <1391801900@qq.com>
2019-06-24 21:22:22 +0800

    加班计算重新整理

--- excel/checking_in.py
+++ excel/checking_in.py
@@ -3,129 +3,14 @@ from datetime import date
 import time
 from xlrd import open_workbook, xldate_as_tuple
 from xlwt import Workbook
-# 返回当天最早一次打卡，最晚一次打卡的元组， 当周一到周五早上打卡早于九点时，让时间等于九点，下班打卡时间早于下午 7:30不计加班时间
-def day_clock_in(name, clock_in_list):
-    data_list = []
-    # 上班时间
-    be_on_duty = min(clock_in_list)
-    # 下班时间
-    off_duty = max(clock_in_list)
 
-    # 取出日期
-    today = time.strptime(be_on_duty, '%Y-%m-%d %H:%M')
-    # 判断日期是周末/法定假日/正常上班
-
-    # 转换上班时间
-    be_on_duty = time.strptime(be_on_duty, '%Y-%m-%d %H:%M')
-    be_on_duty_array = (today[0], today[1], today[2], be_on_duty[3], be_on_duty[4], be_on_duty[5],1,1,0)
-    # 上班打卡时间戳
-    be_on_dutyStamp = int(time.mktime(be_on_duty_array))
-
-    # 转换下班时间
-    off_duty = time.strptime(off_duty, '%Y-%m-%d %H:%M')
-    off_duty_array = (today[0], today[1], today[2], off_duty[3], off_duty[4], off_duty[5],1,1,0)
-    # 下班打卡时间戳
-    off_dutyStamp = int(time.mktime(off_duty_array))
-
-    # 转换成 小时:分钟 的格式，然后进行比较
-    # 上班考勤
-    be_on_duty_checking_in = time.strptime("09:00", "%H:%M")
-    be_on_duty_checking_in_array = (today[0], today[1], today[2], be_on_duty_checking_in[3], be_on_duty_checking_in[4], be_on_duty_checking_in[5],1,1,0)
-    # 上班考勤时间戳
-    be_on_duty_checking_inStamp = int(time.mktime(be_on_duty_checking_in_array))
-
-    # 下班考勤时间
-    off_duty_checking_in = time.strptime("18:00", "%H:%M")
-    off_duty_checking_in_array = (today[0], today[1], today[2], off_duty_checking_in[3], off_duty_checking_in[4], off_duty_checking_in[5],1,1,0)
-    # 下班考勤时间戳
-    off_duty_checking_inStamp = int(time.mktime(off_duty_checking_in_array))
-
-    #开始记加班时间 
-    start_overtime_in = time.strptime("18:30", "%H:%M")
-    start_overtime_in_array = (today[0], today[1], today[2], start_overtime_in[3], start_overtime_in[4], start_overtime_in[5],1,1,0)
-    # 开始记加班时间戳
-    start_overtime_inStamp = int(time.mktime(start_overtime_in_array))
-
-    # 加班餐考勤时间
-    overtime_supper = time.strptime("20:30", "%H:%M")
-    overtime_supper_array = (today[0], today[1], today[2], overtime_supper[3], overtime_supper[4], overtime_supper[5],1,1,0)
-    # 加班餐时间戳
-    overtime_supperStamp = int(time.mktime(overtime_supper_array))
-    
-    # 计算迟到, 迟到时间 = 上班打卡时间 - 上班考勤时间。
-    # 迟到时间大于零记为迟到。当忘记打卡没有补卡的情况下，
-    # 节假日不计迟到
-    
-    # 计算早退，早退时间 = 下班打卡时间 - 下班考勤时间。 
-    # 当忘记打下班卡时，读入的下班卡时间会是上班卡时间，此时不计早退时间
-    # 节假日不计早退
-
-    # 加班时长 = 下班打卡时间 - 开始记加班时间戳
-    # 节假日加班 加班时长 = 下班打卡时间 - 上班打卡时间 - 1小时
-    # 加班时长不足一小时不计加班时间
-    overtime_hours = 0
-    
-    # 周五和周六算加班
-    if today[6] not in  [5, 6] :
-        overtime_hours = off_dutyStamp - start_overtime_inStamp
-    else:
-        overtime_hours = off_dutyStamp - be_on_dutyStamp
-
-    if overtime_hours < 60*60:
-        overtime_hours = 0
-
-    # 取分钟
-    overtime_hours = int(overtime_hours/60)
-    data_list.append(overtime_hours)
-    # 加班餐补，只有当下班卡时间戳大于等于加班餐时间戳才有加班餐
-    supper = (off_dutyStamp-overtime_supperStamp)>=0
-    data_list.append(supper)
-    # 返回时间
-    return data_list
-
-# 单个人每月打卡
-def people_clock_in(name, clock_in_list):
-    day_date_list = []
-    # 取出每天的打卡
-    date_list = []
-    oldDay = ''
-    number_of_days = 0
-    for date_value in clock_in_list:
-        # print(date_value)
-        # 把date_value转换成具体的某一天
-        day = time.strptime(date_value, '%Y-%m-%d %H:%M')
-        dayStr = time.strftime('%Y-%m-%d',day)
-        if oldDay == dayStr:
-            date_list.append(date_value)
-        else:
-            number_of_days += 1
-            if oldDay != '':
-                overtime = day_clock_in(name, date_list)
-                day_date_list.append(overtime)
-            date_list = []
-            oldDay = dayStr
-            date_list.append(date_value)
-           
-        
-    if oldDay != '':
-        overtime = day_clock_in(name, date_list)
-        day_date_list.append(overtime)
-
-    # 当月加班总时长
-    totalOverTime = 0
-    # 当月总餐补
-    totalMeal = 0
-    for element in day_date_list:
-        if element[1]:
-            totalMeal += 1
-        if element[0]:
-            totalOverTime += element[0]
-
-    # 返回值，一个列表0：姓名，1：加班总时长，2：餐补, 3：总出勤天数
-    return [name, totalOverTime, totalMeal, number_of_days]
+if __name__ == '__main__':
+    input_file = './input/杭州源石云科技有限公司_考勤报表_20190226-20190325.xlsx'
+    output_file = './output/杭州源石云科技有限公司_考勤报表_20190226-20190325.xlsx'
+    openexcel(input_file, output_file)
 
 def openexcel(input_file, output_file):
-    headers = ['姓名', '加班时长（分钟）', '加班餐数（次）','总出勤天数（天）']
+    headers = ['姓名', '加班时长（分钟）', '加班餐数（次）','总出勤天数（天）', '迟到次数（次）', '迟到总时间（分钟）']
     input_headers = ['姓名','打卡时间']
     output_workbook = Workbook()
     workOvertimeSheet = output_workbook.add_sheet('加班')
@@ -159,8 +44,152 @@ def openexcel(input_file, output_file):
             for column_index, element in enumerate(row_list):
                 workOvertimeSheet.write(row_index, column_index, element)
         output_workbook.save(output_file)
-if __name__ == '__main__':
+
+# 单个人每月打卡
+def people_clock_in(name, clock_in_list):
+    day_date_list = []
+    # 取出每天的打卡
+    date_list = []
+    oldDay = ''
+    number_of_days = 0
+    for date_value in clock_in_list:
+        # print(date_value)
+        # 把date_value转换成具体的某一天
+        day = time.strptime(date_value, '%Y-%m-%d %H:%M')
+        dayStr = time.strftime('%Y-%m-%d',day)
+
+        # 这线框里面的逻辑需要处理，目前还存在 bug
+        # ----------------------------------------------------------------------------------
+        # 如果是同一天
+        if oldDay == dayStr:
+            date_list.append(date_value)
+            # 不是同一天
+        else:
+            number_of_days += 1
+            if oldDay != '' and len(date_list) > 1:
+                overtime = day_clock_in(name, date_list)
+                day_date_list.append(overtime)
+            date_list = []
+            oldDay = dayStr
+            date_list.append(date_value)
+        # ----------------------------------------------------------------------------------
+           
+    # 一天内打开超过两次才算加班
+    # 这线框里面的逻辑需要处理，目前还存在 bug
+    # ----------------------------------------------------------------------------------
+    if oldDay != '' and len(date_list) > 1:
+        overtime = day_clock_in(name, date_list)
+        day_date_list.append(overtime)
+    # ----------------------------------------------------------------------------------
+
+    # 当月加班总时长
+    totalOverTime = 0
+    # 当月总餐补
+    totalMeal = 0
+    # 迟到总次数
+    beLateCount = 0
+    # 迟到总时间
+    totalBeLateTime = 0
+
+    for element in day_date_list:
+        if element[0]:
+            totalOverTime += element[0]
+        if element[1]:
+            totalMeal += 1
+        if element[2]:
+            beLateCount += 1
+        if element[3]:
+            totalBeLateTime += element[3]
     
-    input_file = './input/杭州源石云科技有限公司_考勤报表_20190226-20190325.xlsx'
-    output_file = './output/杭州源石云科技有限公司_考勤报表_20190226-20190325.xlsx'
-    openexcel(input_file, output_file)
+    # 返回值，一个列表0：姓名，1：加班总时长，2：餐补, 3：总出勤天数, 4:迟到次数, 5:迟到总时间
+    return [name, totalOverTime, totalMeal, number_of_days, beLateCount, totalBeLateTime]
+
+# 返回当天最早一次打卡，最晚一次打卡的元组， 当周一到周五早上打卡早于九点时，让时间等于九点，下班打卡时间早于下午 7:30不计加班时间
+def day_clock_in(name, clock_in_list):
+    data_list = []
+    # 上班时间
+    be_on_duty = min(clock_in_list)
+    # 下班时间
+    off_duty = max(clock_in_list)
+
+    # 取出日期，以上班打卡的日期为准
+    today = time.strptime(be_on_duty, '%Y-%m-%d %H:%M')
+    # 判断日期是周末/法定假日/正常上班
+
+    # 转换上班时间
+    be_on_duty = time.strptime(be_on_duty, '%Y-%m-%d %H:%M')
+    be_on_duty_array = (today[0], today[1], today[2], be_on_duty[3], be_on_duty[4], be_on_duty[5],1,1,0)
+    # 上班打卡时间戳
+    be_on_dutyStamp = int(time.mktime(be_on_duty_array))
+
+    # 转换下班时间
+    off_duty = time.strptime(off_duty, '%Y-%m-%d %H:%M')
+    off_duty_array = (today[0], today[1], today[2], off_duty[3], off_duty[4], off_duty[5],1,1,0)
+    # 下班打卡时间戳
+    off_dutyStamp = int(time.mktime(off_duty_array))
+
+    # 转换成 小时:分钟 的格式，然后进行比较
+    # 当天上班考勤时间
+    be_on_duty_checking_in = time.strptime("09:00", "%H:%M")
+    be_on_duty_checking_in_array = (today[0], today[1], today[2], be_on_duty_checking_in[3], be_on_duty_checking_in[4], be_on_duty_checking_in[5],1,1,0)
+    # 上班考勤时间戳
+    be_on_duty_checking_inStamp = int(time.mktime(be_on_duty_checking_in_array))
+
+    # 当天下班考勤时间
+    off_duty_checking_in = time.strptime("18:00", "%H:%M")
+    off_duty_checking_in_array = (today[0], today[1], today[2], off_duty_checking_in[3], off_duty_checking_in[4], off_duty_checking_in[5],1,1,0)
+    # 下班考勤时间戳
+    off_duty_checking_inStamp = int(time.mktime(off_duty_checking_in_array))
+
+    #开始记加班时间 
+    start_overtime_in = time.strptime("18:30", "%H:%M")
+    start_overtime_in_array = (today[0], today[1], today[2], start_overtime_in[3], start_overtime_in[4], start_overtime_in[5],1,1,0)
+    # 开始记加班时间戳
+    start_overtime_inStamp = int(time.mktime(start_overtime_in_array))
+
+    # 加班餐考勤时间
+    overtime_supper = time.strptime("20:30", "%H:%M")
+    overtime_supper_array = (today[0], today[1], today[2], overtime_supper[3], overtime_supper[4], overtime_supper[5],1,1,0)
+    # 加班餐时间戳
+    overtime_supperStamp = int(time.mktime(overtime_supper_array))
+    # 加班时长
+    overtime_hours = 0
+    # 迟到时长
+    beLateTime = 0
+    # 是否记迟到,1：为记迟到，0：为不记迟到
+    isBeLate = 1
+
+    # 周五和周六算加班，不是节假日
+    # 正常加班 加班时长 = 下班打卡时间 - 开始记加班时间戳
+    if today[6] not in  [5, 6] :
+        # 当天的加班时间
+        overtime_hours = off_dutyStamp - start_overtime_inStamp
+        # 当天的迟到时间, 迟到时间 = 上班打卡时间 - 上班考勤时间。
+        beLateTime = be_on_dutyStamp - be_on_duty_checking_inStamp
+        
+        # 节假日加班 加班时长 = 下班打卡时间 - 上班打卡时间 - 1小时
+    else:
+        # 当天的加班时间
+        overtime_hours = off_dutyStamp - be_on_dutyStamp - 60 * 60
+        # 节假日不算上班迟到。。。
+    
+    # 加班时长不足一小时不计加班时间
+    if overtime_hours < 60*60:
+        overtime_hours = 0
+    
+    # 迟到大于五分钟才计算迟到
+    if beLateTime < 60*5:
+        beLateTime = 0
+        isBeLate = 0
+     # 加班餐补，只有当下班卡时间戳大于等于加班餐时间戳才有加班餐
+    supper = (off_dutyStamp-overtime_supperStamp)>=0
+    # 取分钟
+    overtime_hours = int(overtime_hours/60)
+    beLateTime = int(beLateTime/60)
+
+    data_list.append(overtime_hours)
+    data_list.append(supper)
+    data_list.append(isBeLate)
+    data_list.append(beLateTime)
+    # 返回时间
+    return data_list
